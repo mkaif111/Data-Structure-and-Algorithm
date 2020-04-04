09a36644cc8e598d13c1696a09bf0e59f605355e
Kaif <36125283+thekaif7@users.noreply.github.com>
2020-04-05 00:32:46 +0530

    codechef

--- carsell.cpp
+++ carsell.cpp
@@ -1,1 +1,43 @@
-#include<bits/stdc++.h<
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Mod 1000000007
+#define ll long long int
+
+int main(){
+
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        ll *arr = new ll[n];
+        int count_zero=0;
+        ll sum=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+            // sum = (sum%Mod + arr[i]%Mod)%Mod;
+            // if(arr[i]==0)
+            // count_zero++;
+        }
+        
+        sort(arr,arr+n,greater<int>());
+        ll count=0;
+        for(int i=0;i<n;i++)
+        {   
+            sum =(sum%Mod + max(arr[i]-count,0ll)%Mod)%Mod;
+            count++;
+        }
+
+        cout<<(sum+Mod)%Mod<<endl;
+
+        
+
+
+
+    }
+
+}
