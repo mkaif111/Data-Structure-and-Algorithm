13fd925a2352a9f69e2d5054a0df809604eb9608
Kaif <36125283+thekaif7@users.noreply.github.com>
2020-06-12 20:42:41 +0530

    graph uncomplete

--- graph/knightwalk.cpp
+++ graph/knightwalk.cpp
@@ -1,29 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dfs(int n, int m, int s1 ,int s2 ,int  d1 , int d2,int p_r,int p_c){
-    
+bool pathexist(int s1,int s2, int d1,int d2,int n,int m,bool**visited){
+
+    if(s1==d1 and s2==d2)
+        return true;
+
+    visited[s1][s2] = true;
+
+    int x_step[] = {-2,-2,2,2,-1,1,-1,1};
+    int y_step[] = {1,-1,1,-1,2,2,-2,-2};
+
+    for(int i=0;i<8;i++){
+        int r = s1+x_step[i];
+        int c = s2+y_step[i];
+
+        if(c>=0 && c<m && r>=0 && r<n && !visited[r][c]){
+            if(pathexist(r,c,d1,d2,n,m,visited));
+                return true;
+        }
+            
+    }
+
+    return false;
+
+
+}
+int dfs(int s1,int s2, int d1,int d2,int n,int m,bool**visited){
+
     if(s1==d1 and s2==d2)
         return 0;
-    if(s1>n or s2>m or s1<0 or s2<0)
-        return INT_MAX;
-    if(s1==p_r and s2==p_c)
-        return INT_MAX;
+
+    visited[s1][s2] = true;
+
+    int x_step[] = {-2,-2,2,2,-1,1,-1,1};
+    int y_step[] = {1,-1,1,-1,2,2,-2,-2};
+
+    int count=1000000;
+
+    for(int i=0;i<8;i++){
+        int r = s1+x_step[i];
+        int c = s2+y_step[i];
+
+        if(c>=0 && c<m && r>=0 && r<n && !visited[r][c]){
+            count = min(dfs(r,c,d1,d2,n,m,visited),count);
+            visited[r][c]=false;
+        }
+            
+    }
+    
+
+    return count+1;
 
 
-
-    int option1 = dfs(n,m,s1 - 2,s2+1,d1,d2, s1,s2);
-    int option2 = dfs(n,m,s1 - 2,s2-1,d1,d2, s1,s2);
-    int option3 = dfs(n,m,s1 + 2,s2+1,d1,d2, s1,s2);
-    int option4 = dfs(n,m,s1+2,s2-1,d1,d2, s1,s2);
-    int option5 = dfs(n,m,s1 - 1,s2+2,d1,d2, s1,s2);
-    int option6 = dfs(n,m,s1 + 1,s2+2,d1,d2, s1,s2);
-    int option7 = dfs(n,m,s1 - 1,s2-2,d1,d2, s1,s2);
-    int option8 = dfs(n,m,s1+1,s2-2,d1,d2 ,s1,s2);
-    
-    int ans = min({option1,option2,option3,option4,option5,option6,option7,option8});
-    return ans+1;
-    
-    
 }
 
 int main() {
@@ -37,13 +65,24 @@ int main() {
 	    cin>>n>>m;
 	    int s1,s2,d1,d2;
 	    cin >> s1 >>s2 >>d1 >> d2;
-	    cout<<"great"<<endl;
-	    bool** visited = new bool*[n];
-        cout<<"great"<<endl;
-	   
-	    cout<<"great2"<<endl;
-	    int ans = dfs(n,m,s1-1,s2-1,d1-1,d2-1,s1-1,s2-1);
-	    cout<<ans<<endl;
+        bool **visited = new bool*[n];
+        for(int i=0;i<n;i++){
+            visited[i] = new bool[m]();
+        }
+
+        if(pathexist(s1-1,s2-1,d1-1,d2-1,n,m,visited))
+           {   for(int i=0;i<n;i++){
+               for(int j=0;j<m;j++){
+                   visited[i][j] = false;
+               }
+           }
+               cout<< dfs(s1-1,s2-1,d1-1,d2-1,n,m,visited)<<endl;
+               
+            }
+        else
+            cout<<-1<<endl;
+
+	    
 	}
 	return 0;
 }
