37c2389e0735ab70893f501cbfeeb3a01a74b5b9
Kaif <36125283+thekaif7@users.noreply.github.com>
2020-09-25 20:36:20 +0530

    queue

---
+++ queue/importanttime.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+    
+	int n; cin>>n;
+	queue<int> order;
+	for(int i=0;i<n;i++){
+		int item;
+		cin>>item;
+		order.push(item);
+	}
+	int desire[n];
+	for(int i=0;i<n;i++){
+		cin>>desire[i];
+	}
+	int time = 0;
+	for(int i=0;i<n;i++){
+
+		while(desire[i]!=order.front()){
+			int f = order.front();
+			order.pop();
+			order.push(f);
+			time++;
+		}
+	order.pop();
+	time++;
+	}
+	cout<<time<<"\n";
+
+	return 0;
+}
