caa20499e747bbf0adb5df38931fbaaf07c7a38e
Kaif <36125283+thekaif7@users.noreply.github.com>
2020-11-20 01:03:30 +0530

    codeforce practising

--- Binary Tree/preorder_print.cpp
+++ Binary Tree/preorder_print.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <utility>
+#include<queue>
 using namespace std;
 
 class node
@@ -9,13 +9,12 @@ public:
     int data;
     node *left;
     node *right;
+
     node(int d)
     {
         data = d;
-        left = right = NULL;
     }
 };
-
 node *createTreeFromTrav(int *preorder, int *inorder, int s, int e)
 {
     static int i = 0;
@@ -25,7 +24,7 @@ node *createTreeFromTrav(int *preorder, int *inorder, int s, int e)
     }
 
     node *root = new node(preorder[i]);
-    if(s == e)
+    if (s == e)
         return root;
 
     int index = -1;
@@ -39,11 +38,10 @@ node *createTreeFromTrav(int *preorder, int *inorder, int s, int e)
     }
     i++;
     root->left = createTreeFromTrav(preorder, inorder, s, index - 1);
-    root->right = createTreeFromTrav(preorder, inorder, index+1, e);
+    root->right = createTreeFromTrav(preorder, inorder, index + 1, e);
 
     return root;
 }
-
 // Print level order by BFS
 void BFS(node *root)
 {
@@ -77,17 +75,63 @@ void BFS(node *root)
 
     return;
 }
+void preorder_print(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (root->right != NULL and root->left != NULL)
+    {
+        cout << root->left->data << " => " << root->data << " <= " << root->right->data << endl;
+        return preorder_print(root->left);
+    }
+    if (root->left != NULL and root->right == NULL)
+    {
+        cout << root->left->data << " => " << root->data << " <= "
+             << "END" << endl;
+        return preorder_print(root->left);
+    }
+    if (root->right != NULL and root->left == NULL)
+    {
+        cout << "END"
+             << " => " << root->data << " <= " << root->right->data << endl;
+        return preorder_print(root->right);
+    }
+    if (root->left == NULL and root->right == NULL)
+    {
+        cout << "END"
+             << " => " << root->data << " <= "
+             << "END" << endl;
+        return;
+    }
 
+    return;
+}
 int main()
 {
     int n;
     cin >> n;
     int preorder[n];
-    for (int i = 0; i < n; i++)
-        cin >> preorder[i];
+    for (int j = 0; j < n; j++)
+    {
+        cin >> preorder[j];
+    }
+    cin >> n;
     int inorder[n];
-    for (int i = 0; i < n; i++)
-        cin >> inorder[i];
+    for (int j = 0; j < n; j++)
+    {
+        cin >> inorder[j];
+    }
+    for (int j = 0; j < n; j++)
+    {
+        cout <<"( "<< preorder[j]<<" "<<inorder[j]<<" )"<<" ";
+    }
+    cout<<endl;
     node *root = createTreeFromTrav(preorder, inorder, 0, n - 1);
     BFS(root);
+    
+    preorder_print(root);
+
+    return 0;
 }
